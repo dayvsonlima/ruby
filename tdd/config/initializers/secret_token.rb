c2ae4ed2866e1c31c106e84f7ff18631fcd97fd5
System Administrator <root@new-host.home>
2013-08-08 13:54:25 -0300

    Tdd

---
+++ tdd/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tdd::Application.config.secret_key_base = '457e90de6d1e1d71b1fc2b2e2e3c65f3488f029626116636ca4ccb25992fda81e56fca29a5ff52b0b9776fd492f7bbd618f37dfe20e04d9a87a2a284e04f400a'
